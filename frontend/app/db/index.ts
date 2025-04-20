2a5c67868e9439cf34fb7a65d9978392b80ef69d
anurag bagri <anurag123bagri@gmail.com>
2025-04-20 16:21:46 +0530

    ~skeleton db

--- frontend/app/db/index.ts
+++ frontend/app/db/index.ts
@@ -1,8 +1,18 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from '@prisma/client';
 
- // @ts-ignore
-const prisma  = globalThis.prisma ?? new PrismaClient();
- // @ts-ignore
-if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+type PrismaClientSingleton = ReturnType<typeof prismaClientSingleton>;
+
+// eslint-disable-next-line
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClientSingleton | undefined;
+};
+
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
